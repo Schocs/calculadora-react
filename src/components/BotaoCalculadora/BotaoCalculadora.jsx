7fbed83d4be9656c0275988b706031a5921b7ccc
João Chocron <schocs13@gmail.com>
2023-05-24 18:13:47 -0300

    Pequenas modificações

--- src/components/BotaoCalculadora/BotaoCalculadora.jsx
+++ src/components/BotaoCalculadora/BotaoCalculadora.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './BotaoCalculadora.css';
 
 
-const BotaoCalculadora = ({simbolo, cor, valor, onClick}) => {
+const BotaoCalculadora = ({simbolo, cor, valor, onClick, onKeyPress}) => {
 
     return (
         <div className="botao">
-            <button className="interno" style={{background: cor}} value={valor} onClick={onClick}>
+            <button className="interno" style={{background: cor}} value={valor} onClick={onClick} onKeyDown={onKeyPress}>
                     {simbolo}
             </button>
         </div>

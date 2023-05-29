f552e3a54b28379bf6e150bed4ffa1a236c76bc9
João Chocron <schocs13@gmail.com>
2023-05-29 17:01:10 -0300

    Continuação da limpeza do App

--- src/App.jsx
+++ src/App.jsx
@@ -16,94 +16,9 @@ function App() {
   }
   );
 
-  const loadFromMemoryStorage = () => {
-    let memory = localStorage.getItem('memoryStorage');
-    let history = localStorage.getItem('history').toString();
-    if(memory === 0) {
-      return
-    }
-    setDisplay(memory);
-    setHistory(history);
-  }
-
-  useEffect(() => {
-    loadFromMemoryStorage()
-  }, [])
-
-  const addNumToDisplay = (e) => {
-    const number = e.target.value;
-    if(display === '0') {
-      setDisplay(number);
-    } else {
-      if (number === '.') {
-        setDisplay(display + '.');
-      } else if (number === '-') {
-        setDisplay(display + '-');
-      } else {
-        setDisplay(display + number);
-      }
-    }
-  }
   
-  const addOpToDisplay = (e) => {
-    setDisplay( display + ' ' + e.target.value + ' ' );
-  }
 
-  const delFromDisplay = () => {
-    if (display === '0' || display.length <= 1) {
-      resetDisplay()
-    } else if (display.endsWith(' ')) {
-      setDisplay(display.slice(0, (display.length - 3)))
-    } else {
-      setDisplay(display.slice(0, (display.length - 1)))
-    }
-  }
-
-  const resetDisplay = () => {
-    setDisplay('0');
-    setHistory([]);
-    localStorage.setItem('history', 0)
-    localStorage.setItem('memoryStorage', 0);
-  }
-
-  const calculate = () => {
-    const numbers = display.split(' ');
-    const operator = numbers.pop();
-    if(numbers[1] === '^') {
-      toThePowerOf(numbers[0], operator);
-      return;
-    }
-    const result = eval(`${numbers.join(' ')} ${operator}`);
-    handleResult(result);
-  }
   
-  const handleResult = (result) => {
-    setDisplay(result);
-    setHistory([...history, [display, '=', result, '\n']]);
-    localStorage.setItem('history', history);
-    localStorage.setItem('memoryStorage', result);
-  }
-
-  const sqrt = () => {
-    const num = parseFloat(display);
-    if (isNaN(num)) {
-      return;
-    }
-    handleResult(Math.sqrt(num));
-  }
-
-  const percentage = () => {
-    const num = parseFloat(display);
-    if(isNaN(num)) {
-      return;
-    }
-    handleResult(num/100);
-  }
-
-  const toThePowerOf = (x, y) => {
-    let res = Math.pow(x, y);
-    handleResult(res);
-  }
 
   const HistoryItem = ({ expression, result }) => {
     return (

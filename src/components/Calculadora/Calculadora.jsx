d70c679572d295de7ddf721131b0c1561f605d97
João Chocron <schocs13@gmail.com>
2023-05-30 17:53:15 -0300

    Mudança no estilo(cores). Inicío de função handleTheme, count a ser passado como prop

--- src/components/Calculadora/Calculadora.jsx
+++ src/components/Calculadora/Calculadora.jsx
@@ -54,15 +54,16 @@ const Calculadora = () => {
         }
     }
 
-    const resetDisplay = () => {
+    const reset= () => {
         setDisplay('0');
         setHistory('0');
-        localStorage.setItem('history', history)
-        localStorage.setItem('memoryStorage', display);
+        localStorage.setItem('history', '0')
+        localStorage.setItem('memoryStorage', '0');
     }
 
     const clearDisplay = () => {
         setDisplay('0');
+        localStorage.setItem('memoryStorage', '0');
     }
 
     const sqrt = () => {
@@ -110,9 +111,24 @@ const Calculadora = () => {
         setShowHistory(!showHistory);
     }
 
+    const [count, setCount] = useState(0);
+    const [numCor, setNumCor] = useState('wheat');
+    const [opCor, setOpCor] = useState('coral');
+    const [exCor, setExCor] = useState('cadetblue');
+    const [espCor, setEspCor] = useState('slateblue');
+    const [bodyCor, setBodyCor] = useState('slategrey');
+
+    const handleTheme = () => {
+        switch(count) {
+            case 1: {
+                setNumCor()
+            }
+        }
+    }
+
     return (
         <div className='raiz'>
-        <div className='calcBody'>
+        <div className='calcBody' style={{background: bodyCor}}>
             <div className='painel'>
                 <div className={'display'}>
                     {display}
@@ -121,26 +137,26 @@ const Calculadora = () => {
             <div className='teclado'>
                 <div className='operadores'>
                     {operadores.map(op => (
-                       <BotaoCalculadora cor={'pink'} simbolo={op} key={op} valor={op} onClick={e => addOpToDisplay(e)} />
+                       <BotaoCalculadora cor={opCor} simbolo={op} key={op} valor={op} onClick={e => addOpToDisplay(e)} />
                     ))}
                 </div>
                 <div className='numerico'>
                     {numeros.map(num => (
-                        <BotaoCalculadora cor={'red'} simbolo={num} valor={num} key={num} onClick={e => addNumToDisplay(e)}/>
+                        <BotaoCalculadora cor={numCor} simbolo={num} valor={num} key={num} onClick={e => addNumToDisplay(e)}/>
                         ))}
                 </div>
                 <div className='executores'>
-                    <BotaoCalculadora cor={'grey'} simbolo={'Del'} valor={'del'} onClick={delFromDisplay}/>
-                    <BotaoCalculadora cor={'grey'} simbolo={'bª'} valor={'^'} onClick={e => addOpToDisplay(e)}/>
-                    <BotaoCalculadora cor={'grey'} simbolo={'%'} valor={'%'} onClick={percentage}/>
-                    <BotaoCalculadora cor={'grey'} simbolo={'√'} valor={'√'} onClick={sqrt}/>
+                    <BotaoCalculadora cor={exCor} simbolo={'Del'} valor={'del'} onClick={delFromDisplay}/>
+                    <BotaoCalculadora cor={exCor} simbolo={'bª'} valor={'^'} onClick={e => addOpToDisplay(e)}/>
+                    <BotaoCalculadora cor={exCor} simbolo={'%'} valor={'%'} onClick={percentage}/>
+                    <BotaoCalculadora cor={exCor} simbolo={'√'} valor={'√'} onClick={sqrt}/>
                 </div>
             </div>
             <div className='especiais'>
-                <BotaoCalculadora cor={'blue'} simbolo={'='} valor={'='} onClick={calculate}/>
-                <BotaoCalculadora cor={'blue'} simbolo={'History'} valor={'history'} onClick={isHistoryShown}/>
-                <BotaoCalculadora cor={'blue'} simbolo={'Reset'} valor={'reset'} onClick={resetDisplay}/>
-                <BotaoCalculadora cor={'blue'} simbolo={'Clear'} valor={'clear'} onClick={clearDisplay}/>
+                <BotaoCalculadora cor={espCor} simbolo={'='} valor={'='} onClick={calculate}/>
+                <BotaoCalculadora cor={espCor} simbolo={'History'} valor={'history'} onClick={isHistoryShown}/>
+                <BotaoCalculadora cor={espCor} simbolo={'Reset'} valor={'reset'} onClick={reset}/>
+                <BotaoCalculadora cor={espCor} simbolo={'Clear'} valor={'clear'} onClick={clearDisplay}/>
             </div>
             <PopUp trigger={showHistory} setTrigger={setShowHistory} history={history}/>
         </div>

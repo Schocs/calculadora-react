79d8497b6126baac1537a5656a7e01b22b004643
João Chocron <schocs13@gmail.com>
2023-05-31 13:35:56 -0300

    Adição de props para background do botao

--- src/components/BotaoCalculadora/BotaoCalculadora.jsx
+++ src/components/BotaoCalculadora/BotaoCalculadora.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './BotaoCalculadora.css';
 
 
-const BotaoCalculadora = ({simbolo, cor, valor, onClick, onKeyPress}) => {
+const BotaoCalculadora = ({simbolo, cor, valor, onClick, backCor}) => {
 
     return (
-        <div className="botao">
-            <button className="interno" style={{background: cor}} value={valor} onClick={onClick} onKeyDown={onKeyPress}>
+        <div className="botao" style={{background: backCor}}>
+            <button className="interno" style={{background: cor}} value={valor} onClick={onClick}>
                     {simbolo}
             </button>
         </div>

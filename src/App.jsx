228d76b047a9e32bb2e5ee6ccbd0b8fc1b517cec
João Chocron <schocs13@gmail.com>
2023-06-01 18:31:43 -0300

    Função handleTheme passada para calculadora, seta tema da pag. Pequenas mudanças de estilo, tentar fazer animação

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 import Calculadora from './components/Calculadora/Calculadora';
 import BotaoMudaTema from './components/BotaoMudaTema/BotaoMudaTema';
@@ -7,11 +7,44 @@ function App() {
 
   const [count, setCount] = useState(0)
 
+  const takeCount = () => {
+    if(count == 1) {
+      setCount(4);
+      return
+    }
+    if(count == 0) {
+      setCount(3);
+      return
+    }
+    setCount(count - 1);
+    return
+  }
+
+  const [tema, setTema] = useState('wheat');
+
+  const handleTheme = () => {
+    switch(count) {
+      case 1:
+        setTema('rgb(64, 27, 78)');
+        break;
+      case 2:
+        setTema('rgb(95, 28, 38)')
+        break;
+      case 3:
+        setTema('rgb(130, 155, 124)');
+        break;
+      case 4:
+        setTema('rgb(245, 222, 179)');
+        break;  
+    }
+  }
+
   return (
-    <>
-    <Calculadora className='calc' count={count} setCount={setCount}/>
-    <BotaoMudaTema />
-    </>
+    <div className='appBody' style={{background: tema}} >
+      <BotaoMudaTema transform={'scaleX(-1)'} onClick={takeCount} className='esq' tema={tema}/>
+      <Calculadora className='calc' count={count} setCount={setCount} temaPag={handleTheme} />
+      <BotaoMudaTema onClick={() => setCount(count + 1)} className='dir' tema={tema}/>
+    </div>
   )
 }
 

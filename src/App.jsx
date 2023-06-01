0f9e3e08a707b388adac721ad3e9d260faf83688
João Chocron <schocs13@gmail.com>
2023-06-01 12:35:54 -0300

    Import de botao tema

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import './App.css'
 import Calculadora from './components/Calculadora/Calculadora';
+import BotaoMudaTema from './components/BotaoMudaTema/BotaoMudaTema';
 
 function App() {
 
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <Calculadora className='calc' count={count} setCount={setCount}/>
+    <BotaoMudaTema />
+    </>
   )
 }
 
